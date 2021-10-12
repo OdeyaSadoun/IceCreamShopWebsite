1272ed34a6a085c696a3feb52d65cf9738bd2ff0
אודיה סעדון <osadoun@g.jct.ac.il>
2021-10-12 09:37:15 +0300

    add contnectus

---
+++ IceCreamProject/Models/ConnectUs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamProject.Models
+{
+    public class ConnectUs
+    {
+        public int Id { get; set; }
+    }
+}
