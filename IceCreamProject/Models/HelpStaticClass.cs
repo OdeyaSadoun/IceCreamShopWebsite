23fd5e4e01ecc35f86971bf488e42e9d46360ade
אודיה סעדון <osadoun@g.jct.ac.il>
2021-11-16 21:25:56 +0200

    fff

---
+++ IceCreamProject/Models/HelpStaticClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamProject.Models
+{
+    public class HelpStaticClass
+    {
+        public static string UserName;
+        public static bool IsAdmin = false;
+    }
+}
